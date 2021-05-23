a16a9c71197ffa7f4cd3de5e53004f8da7eade7d
Luca Tonini <luca.r.tonini@gmail.com>
2021-05-24 00:43:41 +0200

    Import towers and their and data files into the resource file

--- resource/Tile Importer/Main.cpp
+++ resource/Tile Importer/Main.cpp
@@ -98,9 +98,144 @@ void doimport(int scale) {
 			Bitmap im;
 			if(im.Import(file_path)) {
 				im = Scale(im, scale);
-
+                
 				auto &imres = *new R::Image(std::move(im));
 				fold.Add(imres);
+                
+                
+                if(fold_name == "Towers"){
+                
+                    auto &data  = *new R::Data;
+                    fold.Add(data);
+
+                    auto name = FS::GetBasename(file_name);
+                    int tid = 0; //Tower ID
+                    int dpb = 0; //damage per bullet
+                    float reloadt = 0.0; //reload time
+                    int bcapacity = 0; //bullet capacity
+                    int cr = 0; //Continuious reload
+                    float bspeed = 0.0;
+                    int damagetype = 0;
+                    float areasize = 0.0;
+                    float damagefalloff = 0.0;
+                    float damagefalloffarea = 0;
+                    int cost = 0;
+                    std::string upgradepath = "";
+                    float range = 0.0;
+                    int placeable = 0;
+                    int target = 0;
+                    float ea = 0.0; //effective against
+                    float em = 0.0; //effective multiplier
+                    float bacc = 0.0; //bullet acceleration
+                    auto startlocation = Point(0, 0);
+                    //auto off  = ;
+
+                    std::ifstream dataf(FS::Join(fold_path, name + ".txt"));
+
+                    if(dataf.is_open()) {
+                        std::string line;
+                        
+                        std::cout << "Found txt file for data" << std::endl;
+
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            tid = String::To<int>(line);
+                        
+                        std::cout << "ID = "<< std::to_string(tid) << std::endl;
+
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            dpb = String::To<int>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            reloadt = String::To<float>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            bcapacity = String::To<int>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            cr = String::To<int>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            bspeed = String::To<float>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            damagetype = String::To<int>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            areasize = String::To<float>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            damagefalloff = String::To<float>(line);
+                            
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            damagefalloffarea = String::To<float>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            cost = String::To<int>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            upgradepath = line;
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            range = String::To<float>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            placeable = String::To<int>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            target = String::To<int>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            ea = String::To<float>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            em = String::To<float>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            bacc = String::To<float>(line);
+                        
+                        std::getline(dataf, line);
+                        if(!line.empty())
+                            startlocation  = String::To<Point>(line);
+                    }
+                    
+                    data.Append("tid", tid);
+                    data.Append("dpb", dpb);
+                    data.Append("reloadt", reloadt);
+                    data.Append("bcapacity", bcapacity);
+                    data.Append("cr", cr);
+                    data.Append("bspeed", bspeed);
+                    data.Append("damagetype", damagetype);
+                    data.Append("areasize", areasize);
+                    data.Append("damagefalloff", damagefalloff);
+                    data.Append("damagefalloffarea", damagefalloffarea);
+                    data.Append("cost", cost);
+                    data.Append("upgradepath", upgradepath);
+                    data.Append("range", range);
+                    data.Append("placeable", placeable);
+                    data.Append("target", target);
+                    data.Append("ea", ea);
+                    data.Append("em", em);
+                    data.Append("bacc", bacc);
+                    data.Append("startlocation", startlocation);
+                }
 
 				std::cout << String::Concat("Imported ", file_name, ".") << std::endl;
 				ind++;
